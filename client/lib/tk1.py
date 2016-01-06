617af46bfd7093967aea17c68aa6dc4d39a09af3
Bo Yan VM <boven.yan@gmail.com>
2016-01-05 22:56:25 -0500

    differenciated tree devices

---
+++ client/lib/tk1.py
@@ -0,0 +1,24 @@
+from wikkit_device import WikkitDevice
+
+
+class TK1(WikkitDevice):
+    def __init__(self, url, dev_id):
+        self.dev_type = 1
+        self.url = url + "tk1/"
+        self.dev_id = dev_id
+
+        super(TK1, self).__init__(self.url, dev_id, 0)
+
+    def route_query(self, query):
+        if super(TK1, self).route_query(query):
+            return True
+
+        element = query.split()
+
+        if (element[0] == "help"):  # print help
+            self._print_help()
+            return True
+
+    def _print_help(self):
+        print "Status: You are controlling tk1-" + str(self.dev_id)
+        super(TK1, self)._print_help()
