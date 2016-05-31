23c97b54e4e9dcfa4c73a6a884af4406e5ae085b
boven <boven.yan@gmail.com>
2016-05-30 23:13:23 -0400

    add ssh key

--- device/auto_deploy.sh
+++ device/auto_deploy.sh
@@ -3,11 +3,21 @@ sudo chown $USER:$USER /opt wikkit
 mkdir -p /opt/wikkit/signal
 cp * /opt/wikkit/signal/
 
+sudo apt-get install python-dev python-pip
+sudo pip install psutil
+
+echo -n "Enter the name of this device"
+read servername
+
 crontab -l > mycron
 echo "@reboot python /opt/wikkit/signal/signal.py" >> mycron
 crontab mycron
 rm mycron
 
+ssh -t rsa -b 4096 -C $servername@wikkit.dev
+
 echo "TODO: edit the config.ini"
 echo "      devID=<id>; devType=<type: pi or tk1>"
-echo "      send ~/.ssh/id_rsa.pub to bo"
+echo "      send the following public key to bo"
+
+echo ~/.ssh/id_rsa.pub
