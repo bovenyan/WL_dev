e3939696d59d01536dc2af45e4154b2dc314af04
Bo Yan VM <boven.yan@gmail.com>
2016-06-07 17:12:35 -0400

    bug fix

--- app/test_server.py
+++ app/test_server.py
@@ -1,6 +1,83 @@
-from flask import request, jsonify, abort
+from flask import request, jsonify, abort, Flask
+from datetime import datetime, timedelta
+import string
+import random
 
 app = Flask(__name__)
 
-@app.route("/upload"):
+active_transmission = {}
+max_concurrent_trans = 5
+kill_timeout = 1800
+chunk_path = "/tmp/night-chunks/"
+file_path = "/tmp/night-files/"
+from subprocess import call
 
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def clear_inactive_transmission():
+    for idx in range(len(active_transmission)):
+        if (datetime.now() - active_transmission[key] >
+            timedelta(0, kill_timeout, 0)):
+            del active_transmission[key]
+
+def cal_chksum(file_name):
+    digester = hashlib.md5()
+    with open(file_name, "rb") as f:
+        for piece in iter(lambda: f.read(4096), b""):
+            digester.update(piece)
+    return digester.hexdigest()
+
+@app.route("/night/<int:devId>/req_start", methods=['GET'])
+def req_start(devId):
+    if len(active_transmission) >= max_concurrent_trans:
+        clear_inactive_transmission()
+
+    if len(active_transmission) < max_concurrent_trans:
+        chunk_path_dev = chunk_path + str(devId)
+        call(["mkdir", "-p", chunk_path_dev])
+        call(["rm", chunk_path_dev + "/*"])
+
+        filename = id_generator()
+        active_transmission[filename] = datetime.now()
+        return jsonify({"filename": filename})
+    else:
+        return jsonify({})
+
+@app.route("/night/<int:devId>/send_chunk", methods=['POST'])
+def send_chunks(devId):
+    if ('file' not in requests.files):
+        abort(400)
+    chunk = requests.files['file']
+    chunk_path_dev = chunk_path + str(devId)
+
+    chksum = request.form.get['chksum']
+    chunkname = request.form.get['chunkname']
+
+    chunk.save(os.path.join(chunk_path_dev, chunkname))
+
+    chksum_cal = cal_chksum(chunk, chunk_path_dev + "/" + chunkname)
+
+    if chksum == chksum_cal:
+        return jsonify({})
+    else:
+        return jsonify({"fail_code": 1})
+
+@app.route("/night/<int:devId>/send_chunk", methods=['POST'])
+def confirm_done(devId):
+    if (requests.json() is None or "filename" not in requests.json()):
+        abort(400)
+
+    file_path_dev = file_path + str(devId)
+    call(["mkdir", "-p", file_path_dev])
+    call(["cat", chunk_path + filename + "*", ">", file_path + filename])
+
+    del active_transmission[filename]
+
+@app.route("/night/<int:devId>/info_kill", methods=['POST'])
+def info_kill(devId):
+    chunk_path_dev = chunk_path + str(devId) + "/*"
+    call(["rm", "-r", chunk_path_dev])
+    print("device " + str(devId) + "too slow ... killed"
--- app/uploader.py
+++ app/uploader.py
@@ -13,39 +13,67 @@ headers = {'Content-Type': 'application/json'}
 
 def request_start(url, devId):
     url = url + str(devId) + "/req_start"
-    response = requests.get(url, headers=headers)
+    try:
+        response = requests.get(url, headers=headers)
+        if (response.ok and not (response.json() is None) and
+           "filename" in response.json()):
+            res = response.json()
+            return res["filename"]
 
-    if (response.ok and not (response.json() is None)):
-        res = response.json()
-        return res["token"]
+    except Exception, e:
+        print "error requesting start " + str(e)
+        return None
 
     return None
 
 
-def split_file(file_path, chunk_path, token, split_len):
+def inform_kill(url, devId):
+    url = url + str(devId) + "/info_kill"
+
+    try:
+        requests.post(url)
+    except Exception, e:
+        print "error informing kill " + str(e)
+
+
+def split_file(file_path, chunk_path, filename, split_len):
     call(["mkdir", "-p", chunk_path])
     call(["rm", "-r", chunk_path+"/*"])
-    call(["split", file_path, "--bytes="+split_len+"M", chunk_path+"/"+token])
-    # call(["cat", token+"*", ">", token])
+    call(["split", file_path,
+          "--bytes="+split_len+"M", chunk_path+"/"+filename])
 
 
-def send_chunks(url, devId, chunk_path, success_backoff, failure_backoff):
-    url = url + str(devId) + "/send_chunks"
+def cal_chksum(file_name):
+    digester = hashlib.md5()
+    with open(file_name, "rb") as f:
+        for piece in iter(lambda: f.read(4096), b""):
+            digester.update(piece)
+    return digester.hexdigest()
+
+
+def confirm_done(url, devId):
+    url_confirm = url + str(devId) + "/confirm"
+
+    try:
+        requests.post(url_confirm)
+    except Exception, e:
+        print "error confirming " + str(e)
+
+
+def send_chunks(url, devId, chunk_path, filename,
+                success_backoff, failure_backoff):
+    url_send = url + str(devId) + "/send_chunks"
 
     idx = 0
 
     for subdir, dirs, files in os.walk("/tmp/parts"):
         while idx < len(files):
-            digester = hashlib.md5()
-            with open(files[idx], "rb") as f:
-                for piece in iter(lambda: f.read(4096), b""):
-                    digester.update(piece)
-            chksum = digester.hexdigest()
-
             files = {'file': open(files[idx], 'rb')}
             try:
-                response = requests.post(url, files=files,
-                                         data={"chksum": chksum})
+                chksum = cal_chksum(files[idx])
+                response = requests.post(url_send, files=files,
+                                         data={"chksum": chksum,
+                                               "chunkname": files[idx].name})
                 if not response.ok:
                     print "chunk " + str(files[idx].name) + \
                         " failed, retransmit..."
@@ -61,6 +89,8 @@ def send_chunks(url, devId, chunk_path, success_backoff, failure_backoff):
             except Exception, e:
                 print "error happens for file: " + str(e)
                 sleep(failure_backoff)
+
+    confirm_done(url, devId, filename)
 
 
 if __name__ == "__main__":
@@ -80,14 +110,15 @@ if __name__ == "__main__":
     # TODO: the following piece of program should be integrated in program
     split_file(url)
 
-    token = None
+    filename = None
 
-    while token is None:
-        token = request_start(url, devId)
-        if token is None:
+    while filename is None:
+        filename = request_start(url, devId)
+        if filename is None:
             sleep(failure_backoff)
 
     proc = Process(target=send_chunks, args=(url, devId, chunk_path,
+                                             filename,
                                              success_backoff,
                                              failure_backoff))
     proc.start()
@@ -98,5 +129,4 @@ if __name__ == "__main__":
         print "Upload failed to accomplish in " + str(kill_timeout) + " sec"
         print "Maybe network is too slow.. Killed.."
         proc.terminate()
-
-    send_chunks(url, chunk_path)
+        inform_kill(url, devId)
